978ce4b5fa513fa8182da430c9dadfdd9e22e7b8
Kelly Beshara-Flynn <wavewitch@cox.net>
2017-01-29 13:47:49 -0500

    some mixed up stuff that I tried to copy when Dominic was typing

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import webapp2
 import random
 
+#html boilerplate
+
+page_header = """
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Flicklist</title>
+    </head>
+    <body>
+    <h1>Flicllist</h1>
+    </body>
+<html>"""
+
 class Index(webapp2.RequestHandler):
+
 
     def getRandomMovie(self):
 
@@ -14,6 +28,19 @@ class Index(webapp2.RequestHandler):
         return random.choice(movie_list)
 
     def get(self):
+
+        edit_header = "<h3>Edit My Watchlist</h3>"
+
+        add_form - """
+        <form attribute="/add" method="post">
+            <label>
+                I want to add
+                <input type="text" name="new-movie"/>
+                to mywatchlist
+            </lable>
+            <input type="submit" name="Add It"/>
+        </form>"""
+
         # choose a movie by invoking our new function
         movie = self.getRandomMovie()
 
@@ -30,6 +57,9 @@ class Index(webapp2.RequestHandler):
 
         self.response.write(content2 + content)
 
+
+
 app = webapp2.WSGIApplication([
     ('/', Index)
+    ('/add', AddMovie)
 ], debug=True)
